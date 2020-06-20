afe1bdda047beeb8b08dbfb6990a52592577c873
Mario Jorge <mariojgmaster@yahoo.com.br>
2020-06-20 15:37:10 -0300

    Creating Routes

--- StoreAPI/v1/app.js
+++ StoreAPI/v1/app.js
@@ -1,10 +1,50 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+// ROTAS
+const rotaProdutos = require('./routes/produtos');
+const rotaPedidos = require('./routes/pedidos');
+
+// GERADOR DE LOGS
+app.use(morgan('dev'));
+
+app.use(bodyParser({ extended: false }));
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Ok, deu certo!'
+    res.header('Access-Controll-Allow-Origin', '*');
+    res.header(
+        'Access-Controll-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Controll-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE', 'GET');
+        return res.status(200).send({});
+    }
+
+    next();
+})
+
+app.use('/produtos', rotaProdutos);
+app.use('/pedidos', rotaPedidos);
+
+// NÃO ENCONTROU ROTA
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
     })
-});
+})
+
 
 module.exports = app;
---
+++ StoreAPI/v1/routes/pedidos.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+// RETORNA TODOS OS PEDIDOS
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: 'Retorna os pedidos'
+    });
+});
+
+// INSERE UM PEDIDO
+router.post('/', (req, res, next) => {
+    const pedido = {
+        id_produto: req.body.id_produto,
+        quantidade: req.body.quantidade
+    }
+    res.status(201).send({
+        mensagem: 'O pedido foi criado',
+        pedidoCriado: pedido
+    });
+});
+
+// RETORNA OS DADOS DE UM PEDIDO
+router.get('/:id_pedido', (req, res, next) => {
+    const id = req.params.id_pedido;
+    res.status(200).send({
+        mensagem: 'Detalhes do pedido',
+        id_pedido: id
+    });
+});
+
+// REMOVE UM PEDIDO
+router.delete('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: 'Pedido excluido'
+    });
+});
+
+
+module.exports = router;
